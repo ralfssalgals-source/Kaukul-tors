d3a4894fd745878c9805f5572ddfb0add2d25c25
ralfssalgals-source <ralfs.salgals@gmail.com>
2026-03-31 09:51:39 +0300

    Update kaukulators.py

--- kaukulators.py
+++ kaukulators.py
@@ -13,6 +13,7 @@ def btnClick(number):
     E.insert(0, newNumber) # ievieto no 0 lidz newnumber
     return 0
 
+
 def btnCommand(command):
     global number
     global mathOp
@@ -52,33 +53,71 @@ def Clear():
     return 0 
    
 
+def kvad():
+    global operator
+    global num1
+    global mathOp
+    num1 = (float(E.get())**2)
+    E.delete(0, END)
+    E.insert(0, num1)
+    return 0 
+
+
+def kvadratSk():
+    global operator
+    global num1
+    global mathOp
+    num1 = (float(E.get()))
+    num1 = sqrt(num1)
+    E.delete(0, END)
+    E.insert(0, num1)
+    return 0 
+
+
+def PlusMin():
+    global operator
+    global num1
+    global mathOp
+    num1 = -(float(E.get()))
+    E.delete(0, END)
+    E.insert(0, num1)
+    return 0 
+
+
+def logy():
+    global operator
+    global mathOp
+    global num1
+    num1 = (float(E.get()))
+    num1=log(num1)
+    E.delete(0, END)
+    E.insert(0, str(num1))
+    return 0
 
 
 
-btn0 = Button(manslogs, text="0", padx="40", pady="20", bd= 3, command=lambda:btnClick(0))
-btn1 = Button(manslogs, text="1", padx="40", pady="20", bd= 3, command=lambda:btnClick(1))
-btn2 = Button(manslogs, text="2", padx="40", pady="20", bd= 3, command=lambda:btnClick(2))
-btn3 = Button(manslogs, text="3", padx="40", pady="20", bd= 3, command=lambda:btnClick(3))
-btn4 = Button(manslogs, text="4", padx="40", pady="20", bd= 3, command=lambda:btnClick(4))
-btn5 = Button(manslogs, text="5", padx="40", pady="20", bd= 3, command=lambda:btnClick(5))
-btn6 = Button(manslogs, text="6", padx="40", pady="20", bd= 3, command=lambda:btnClick(6))
-btn7 = Button(manslogs, text="7", padx="40", pady="20", bd= 3, command=lambda:btnClick(7))
-btn8 = Button(manslogs, text="8", padx="40", pady="20", bd= 3, command=lambda:btnClick(8))
-btn9 = Button(manslogs, text="9", padx="40", pady="20", bd= 3, command=lambda:btnClick(9))
+btn0 = Button(manslogs, text="0", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(0), bg='gray', height= 2, width= 3)
+btn1 = Button(manslogs, text="1", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(1), bg='gray', height= 2, width= 3)
+btn2 = Button(manslogs, text="2", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(2), bg='gray', height= 2, width= 3)
+btn3 = Button(manslogs, text="3", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(3), bg='gray', height= 2, width= 3)
+btn4 = Button(manslogs, text="4", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(4), bg='gray', height= 2, width= 3)
+btn5 = Button(manslogs, text="5", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(5), bg='gray', height= 2, width= 3)
+btn6 = Button(manslogs, text="6", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(6), bg='gray', height= 2, width= 3)
+btn7 = Button(manslogs, text="7", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(7), bg='gray', height= 2, width= 3)
+btn8 = Button(manslogs, text="8", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(8), bg='gray', height= 2, width= 3)
+btn9 = Button(manslogs, text="9", padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnClick(9), bg='gray', height= 2, width= 3)
 
 
-btnSum = Button(manslogs, text= '+', padx="40", pady="20", bd= 3, command=lambda:btnCommand("+"))
-btnMin = Button(manslogs, text= '-', padx="40", pady="20", bd= 3, command=lambda:btnCommand("-"))
-btnTim = Button(manslogs, text= '*', padx="40", pady="20", bd= 3, command=lambda:btnCommand("*"))
-btnDiv = Button(manslogs, text= '/', padx="40", pady="20", bd= 3, command=lambda:btnCommand("/"))
-btnEquel = Button(manslogs, text= '=', padx="40", pady="20", bd= 3, command=Vienads)
-btnCom = Button(manslogs, text= '.', padx="40", pady="20", bd= 3)
-btnClear = Button(manslogs, text= 'c', padx="40", pady="20", bd= 3, command=Clear)
-btnMinFy = Button(manslogs, text= '-( )', padx="40", pady="20", bd= 3)
-btnWallL = Button(manslogs, text= '(', padx="40", pady="20", bd= 3)
-btnWallR = Button(manslogs, text= ')', padx="40", pady="20", bd= 3)
-btnSquer = Button(manslogs, text= '^2', padx="40", pady="20", bd= 3)
-btnSquerRoot = Button(manslogs, text= '^0.5', padx="40", pady="20", bd= 3)
+btnSum = Button(manslogs, text= '+', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnCommand("+"), bg='light gray', height= 2, width= 3)
+btnMin = Button(manslogs, text= '-', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnCommand("-"), bg='light gray', height= 2, width= 3)
+btnTim = Button(manslogs, text= '*', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnCommand("*"), bg='light gray', height= 2, width= 3)
+btnDiv = Button(manslogs, text= '/', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=lambda:btnCommand("/"), bg='light gray', height= 2, width= 3)
+btnEquel = Button(manslogs, text= '=', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=Vienads, bg='dark gray', fg= 'green', height= 2, width= 3)
+btnlog = Button(manslogs, text= 'log', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=logy, bg='light gray', height= 2, width= 3)
+btnClear = Button(manslogs, text= 'c', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=Clear, bg='dark gray', fg='red', height= 2, width= 3)
+btnMinFy = Button(manslogs, text= '+/-', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=PlusMin, bg='light gray', height= 2, width= 3)
+btnSquer = Button(manslogs, text= '^2', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=kvad, bg='light gray', height= 2, width= 3)
+btnSquerRoot = Button(manslogs, text= '^0.5', padx="40", pady="20", bd= 3, font=("Arial Black", 12), command=kvadratSk, bg='light gray', height= 2, width= 3)
 
 
 btn0.grid(row=5, column= 2)
@@ -98,15 +137,13 @@ btnMin.grid(row=3, column= 4)
 btnTim.grid(row=2, column= 4)
 btnDiv.grid(row=1, column= 4)
 btnEquel.grid(row=5, column= 4)
-btnCom.grid(row=5, column= 3)
+btnlog.grid(row=5, column= 3)
 btnClear.grid(row=1, column= 1)
 btnMinFy.grid(row=5, column= 1)
-btnWallL.grid(row=1, column= 2)
-btnWallR.grid(row=1, column= 3)
 btnSquer.grid(row=6, column= 1)
 btnSquerRoot.grid(row=6, column= 2)
 
-E = Entry(manslogs, width=15, bd= 5, font=("Arial Black", 20))
+E = Entry(manslogs, width=20, bd= 5, font=("Arial Black", 20))
 E.grid(row=0, column=1, columnspan=4)
 
 manslogs.mainloop()
